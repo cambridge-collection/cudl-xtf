15bb0a4c08aecbccf920bd4ec563b97cbf830e6d
Hal Blackburn <hwtb2@cam.ac.uk>
2021-03-19 08:58:44 +0000

    feat(Docker): allow defining indexes via envars

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 confd -log-level ERROR -onetime -backend env || { echo "Error: Templating config files with environment variables failed" >&2 && exit 1; }
+
+# Create directories that need to exist. For example, XTF expects the dir it
+# writes indexes to to exist.
+xargs < /opt/xtf/conf/required-dirs.txt mkdir -p
+
 exec "$@"
